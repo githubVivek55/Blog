067f86be23e8d6e52b6fa34608dafb61a58749d4
githubVivek55 <vivekpce55@outlook.com>
2022-07-22 23:00:10 +0800

    Add author and comment form component

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -10,12 +10,13 @@ import { getPostDetails, getPosts } from "../../services";
 import { Post } from "../../types/post";
 
 const PostDetails = ({ post }: { post: Post }) => {
+  console.log(post);
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           <PostDetail post={post} />
-          <Author author={post.author} />
+          <Author author={post.auther} />
           <CommentsForm slug={post.slug} />
           <Comments slug={post.slug} />
         </div>
